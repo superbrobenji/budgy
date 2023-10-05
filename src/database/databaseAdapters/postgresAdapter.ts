9bfe828325c65ed133f194e4f2f2aad228061795
benji <bennie.h.swanepoel@gmail.com>
2023-10-05 21:04:42 +0200

    cleaned up the db adapter

--- src/database/databaseAdapters/postgresAdapter.ts
+++ src/database/databaseAdapters/postgresAdapter.ts
@@ -3,15 +3,30 @@ import DB from '../dbInterface';
 const Pool = require('pg').Pool;
 
 export default class PostgresAdapter implements DB {
-    private readonly pool: typeof Pool;
+    private pool: typeof Pool;
+    
+    private readonly user: string;
+    private readonly host: string;
+    private readonly database: string;
+    private readonly password: string;
+    private readonly port: number;
 
     public constructor(user: string, host: string, database: string, password: string, port: number) {
+        this.user = user;
+        this.host = host;
+        this.database = database;
+        this.password = password;
+        this.port = port;
+
+        this.connectDb();
+    }
+    private connectDb() {
         this.pool = new Pool({
-            user: user,
-            host: host,
-            database: database,
-            password: password,
-            port: port,
+            user: this.user,
+            host: this.host,
+            database: this.database,
+            password: this.password,
+            port: this.port,
         });
     }
     async query(sql: string, params: any) {
