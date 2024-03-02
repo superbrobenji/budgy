3526e038a19dd63d147efa8f34a3c5c0ea768bf6
benji <bennie.h.swanepoel@gmail.com>
2024-03-02 12:53:49 +0200

    removed cdk

--- cmd/main.go
+++ cmd/main.go
@@ -3,37 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/jsii-runtime-go"
-	"github.com/superbrobenji/budgy/infrastructure/cdk"
+	transport "github.com/superbrobenji/budgy/infrastructure/transport/http"
 )
 
 func main() {
-
-	defer jsii.Close()
-
-	app := awscdk.NewApp(nil)
-
-	cdk.NewGoCdkStack(app, "GoCdkStack", &cdk.GoCdkStackProps{
-		StackProps: awscdk.StackProps{
-			Env: cdk.Env(),
-		},
-	})
-
-	app.Synth(nil)
 	// Create a new request multiplexer
 	// Take incoming requests and dispatch them to the matching handlers
-	mux := http.NewServeMux()
-
-	// Register the routes and handlers
-	mux.Handle("/", &homeHandler{})
-
+	mux := transport.NewServer()
 	// Run the server
 	http.ListenAndServe(":8080", mux)
-}
-
-type homeHandler struct{}
-
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my home page"))
 }
---
+++ infrastructure/transport/http/http.go
@@ -0,0 +1,20 @@
+package transport
+
+import "net/http"
+
+func NewServer() (mux *http.ServeMux) {
+	// Create a new request multiplexer
+	// Take incoming requests and dispatch them to the matching handlers
+	muxServer := http.NewServeMux()
+
+	// Register the routes and handlers
+	muxServer.Handle("/", &homeHandler{})
+
+	return muxServer
+}
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("This is my home page"))
+}
