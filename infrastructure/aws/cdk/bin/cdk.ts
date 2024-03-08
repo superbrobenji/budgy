b05fff2b775b9a7f84d9970fc495ba69b7eb7328
benji <bennie.h.swanepoel@gmail.com>
2024-03-08 19:30:44 +0200

    added env file

--- infrastructure/aws/cdk/bin/cdk.ts
+++ infrastructure/aws/cdk/bin/cdk.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import * as dotenv from 'dotenv'
 import { BudgyStack } from '../lib/cdk-stack';
 
+dotenv.config()
 const app = new cdk.App();
 console.log(process.env.VERSION)
 console.log(process.env.AWS_ACCOUNT)
